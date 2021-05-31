3128686c64a6a44738f1b4178712931be6829869
daein <dnfwlxo11@naver.com>
2021-05-31 17:23:37 +0900

    add: reload event code

--- public/js/main.js
+++ public/js/main.js
@@ -47,7 +47,7 @@ function extractURL() {
 
 function enterRoom() {
     const roomName = decodeURI(extractURL())
-    socket.emit('joinRoom_chat', { room: roomName, stat: 'chat'});
+    socket.emit('joinRoom_chat', { room: roomName, stat: 'chat' });
     roomTitle.innerText = roomName;
 }
 
@@ -55,13 +55,27 @@ function userNum() {
     socket.emit('getUserNum');
 }
 
-function init() {    
+// 새로고침 처리
+document.onkeydown = (e) => {
+    /* F5, Ctrl+r, Ctrl+F5 */
+    if (e.keyCode == 116 || e.ctrlKey == true && (e.keyCode == 82)) {
+        e.cancelBubble = true;
+        e.returnValue = false;
+        socket.emit('leave');
+        setTimeout(function () {
+            window.location.reload();
+        }, 0);
+        return false;
+    }
+}
+
+function init() {
     enterRoom();
     userNum();
 
     leave_btn.addEventListener('click', () => {
         socket.emit('leave');
-        window.history.back();
+        window.location.href = 'http://localhost:3000/'
     })
 }
 
